3f0728b66c8dbc9e5538f2667d6bbcecf2bc419a
ombhonsle16 <ombhonsle16@gmail.com>
2022-06-02 08:26:06 +0530

    Practical 7

---
+++ app/src/main/java/com/example/practical7/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.practical7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextView t1;
+    TextView t2;
+    TextView t3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
+        t1=(TextView)findViewById(R.id.t1);
+        t2=(TextView)findViewById(R.id.t2);
+        t3=(TextView)findViewById(R.id.t3);
+    }
+    public void Display(View view) {
+        t3.setText("LOGIN SUCCESSFUL");
+    }
+}
